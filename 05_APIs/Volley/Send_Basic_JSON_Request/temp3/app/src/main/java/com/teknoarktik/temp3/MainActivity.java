3cfd6e45ff22f42c454c2d42f51e24c0a8a98d2d
Mustafa <mustafakatipoglu98@hotmail.com>
2019-08-02 01:27:08 +0300

    Send Basic JSON Request using Volley API

---
+++ 05_APIs/Volley/Send_Basic_JSON_Request/temp3/app/src/main/java/com/teknoarktik/temp3/MainActivity.java
@@ -0,0 +1,47 @@
+package com.teknoarktik.temp3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+
+        String url = "https://jsonplaceholder.typicode.com/todos/1";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("JSON", "onResponse" + response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("JSON", "onResponse" + error);
+                        error.printStackTrace();
+                    }
+                });
+
+        requestQueue.add(jsonObjectRequest);
+        requestQueue.start();
+    }
+}
